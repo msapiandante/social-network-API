507f8b194ec596b6b1d53a42d232a22139e96f02
Melanie S <melanie.sapiandante@gmail.com>
2023-05-15 21:59:00 -0400

    added framework as per readme guidelines

---
+++ models/user.js
@@ -0,0 +1,17 @@
+// * `username`
+//     * String
+//     * Unique
+//     * Required
+//     * Trimmed
+
+//     * `email`
+//     * String
+//     * Required
+//     * Unique
+//     * Must match a valid email address(look into Mongoose's matching validation)
+
+//         * `thoughts`
+//         * Array of`_id` values referencing the`Thought` model
+
+//         * `friends`
+//     * Array of`_id` values referencing the`User` model(self - reference)
